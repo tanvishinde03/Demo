aaa3d80cf75b8c4238aa0540f79fff1144b9a6d0
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-02 08:41:26 +0530

    Tanvi

---
+++ Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace demo
+{
+   public  class UC1
+    {
+       public  static void MyMethodUC1()
+        {
+            Console.WriteLine("Hello World and WELCOME!");
+     
+        }
+
+
+       
+    }
+
+}
---
+++ Demo/UC1.cs
@@ -0,0 +1,23 @@
+﻿using demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+  
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            UC1 Myobj = new UC1();
+            Myobj.MyMethodUC2();
+            UC1.MyMethodUC1();
+                
+            }
+
+
+        }
+    }
